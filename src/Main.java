ca95abe4c1062b6112a3c2de0bef0d035ed2f28a
Dimassis <166415819+Dimassis@users.noreply.github.com>
2024-09-05 22:36:46 +0500

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task.Task_1();
+        Task.Task_2();
+        Task.Task_3();
+        Task.Task_4();
     }
 }
---
+++ src/Task.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Task {
+    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    static Set<Integer> nums2 = new HashSet<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    static Set<String> strings3 = new HashSet<>(List.of("один", "два","два", "три", "три", "три"));
+    static List<String> strings4 = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+
+    public static void Task_1() {
+        for (Integer i : nums) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+        for (Integer i : nums) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void Task_2() {
+            for (Integer i : nums2) {
+                if ((i % 2 == 0)) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+    }
+
+    public static void Task_3() {
+        for (String s : strings3) {
+            System.out.print(s +" ");
+        }
+        System.out.println();
+    }
+
+    public static void Task_4() {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : strings4) {
+            if (!map.containsKey(s)) {
+                map.put(s, 1);
+            } else {
+                map.put(s, map.get(s) + 1);
+            }
+        }
+        for(String key : map.keySet()) {
+            System.out.print(map.get(key) + " ");
+        }
+    }
+}
